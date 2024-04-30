6e52985dfb6de60dd1849a8c15069241e977c7b8
03Jesus <saavedraj@utb.edu.co>
2024-04-29 23:18:01 -0500

    Adding comment to trigger deployment

--- clients/main.py
+++ clients/main.py
@@ -13,7 +13,7 @@ async def app_lifespan(app: FastAPI):
     # code to execute when app is shutting down
     print("Shutting down app")
 
-# Main app v0.0.1
+# Main app  v0.0.1
 app = FastAPI(version="0.0.1",lifespan=app_lifespan)
 
 app.add_middleware(
